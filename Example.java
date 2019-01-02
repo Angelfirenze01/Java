92e7cd1b62dd38d76ca2bcd0eb5524ab6ffccdc4
Angelfirenze <intothecode01@tutanota.com>
2019-01-02 18:40:19 -0500

    Add Example.java to Java project.
    Signed-off-by: Angelfirenze <intothecode01@tutanota.com>

--- Example.java
+++ Example.java
@@ -2,7 +2,10 @@ public class Example {
  
   public static void main(String[] args) {
     // Your amazing code goes here...
-    
+      System.out.println("We are making a new PEZ dispenser");
+    PezDispenser dispenser = new PezDispenser ();
+    System.out.printf("The dispenser is %s %n",
+                       dispenser.characterName);
   }
 
 }
