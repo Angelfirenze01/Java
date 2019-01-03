26872f9d3eef8a35ec3532280b483cb0c6156bcb
Angelfirenze <intothecode01@tutanota.com>
2019-01-02 19:40:22 -0500

    Work on Java Objects.
    Signed-off-by: Angelfirenze <intothecode01@tutanota.com>

--- Example.java
+++ Example.java
@@ -4,8 +4,9 @@ public class Example {
     // Your amazing code goes here...
       System.out.println("We are making a new PEZ dispenser");
     PezDispenser dispenser = new PezDispenser ();
+    dispenser.characterName = "Darth Vader";
     System.out.printf("The dispenser is %s %n",
-                       dispenser.characterName);
+    				dispenser.characterName);
   }
 
 }
