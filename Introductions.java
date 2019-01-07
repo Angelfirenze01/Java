c49c250d8996047524320e41b00a9c972b851eab
Angelfirenze <intothecode01@tutanota.com>
2019-01-07 08:52:47 -0500

    Update Introductions Java file.
    Signed-off-by: Angelfirenze <intothecode01@tutanota.com>

--- Introductions.java
+++ Introductions.java
@@ -1,9 +1,13 @@
 import java.io.Console;
-
+ 
 public class Introductions {
-	class Introductions {
-	Console console = System.console();
-	// Welcome to the Introductions program! Your code goes below here
-	console.printf("Hello, my name is Nicole");
+  
+    public static void main(String[] args) {
+        Console console = System.console();
+        // Welcome to the Introductions program!  Your code goes below here
+      String firstName = console.readLine("What is your name? ");
+      //thisIsAnExampleOfCamelCasing
+        console.printf("Hello, my name is %s\n", firstName);
+        console.printf("%s is learning how to write Java\n", firstName);
   }
 }
